7983370624fccc283140775ce5212e9fa3d76de5
Johannes Neumeier <hello@johannesneumeier.com>
2014-11-01 21:40:30 +0200

    added potentiometers to circuit and arduino sketch

--- arduino-serial/arduino-serial.ino
+++ arduino-serial/arduino-serial.ino
@@ -6,10 +6,25 @@ void setup() {
 }
 
 void loop() {
+  int a0 = analogRead(A0);
+  int a1 = analogRead(A1);
+  int a2 = analogRead(A2);
   int a3 = analogRead(A3);
   int a4 = analogRead(A4);
   int a5 = analogRead(A5);
+  
+  Serial.println("---------------");
+  
+  Serial.println("Potentiometers:");
   Serial.println(a3);
   Serial.println(a4);
   Serial.println(a5);
+  
+  Serial.println("Photoresistors:");
+  Serial.println(a0);
+  Serial.println(a1);
+  Serial.println(a2);
+  
+  
+  delay(10);
 }
